0a2597a93de4a768baa88a1bfccebcdc5c9b33ea
DocDrewToo <90295104+DocDrewToo@users.noreply.github.com>
2021-09-14 19:09:13 -0400

    Update __main__.py

--- project/__main__.py
+++ project/__main__.py
@@ -1,1 +1,8 @@
+import logging
+import os
 
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "info")
+# Logging Setup
+LOG_LEVEL = getattr(logging, LOG_LEVEL.upper(), logging.ERROR)
+logging.basicConfig(format="%(asctime)s %(name)s %(levelname)s %(message)s")
+logging.getLogger().setLevel(LOG_LEVEL)
